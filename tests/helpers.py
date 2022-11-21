194ac69cffb4cb3b4f31c7d9bf45f31e9d42b61f
Ondra Krejčí <o.krejci13@gmail.com>
2022-11-21 18:03:32 +0100

    Helper to check partial JSON equality

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,6 +1,7 @@
 
 from flask.testing import FlaskClient
 from werkzeug.test import TestResponse
+from deepdiff.diff import DeepDiff
 
 from http import HTTPStatus
 from json import loads
@@ -29,3 +30,13 @@ def login(client: FlaskClient, email: str, password: str) -> None:
 def logout(client: FlaskClient) -> None:
 	resp = client.post('/logout')
 	assert resp.status_code == HTTPStatus.OK
+
+def assert_dict_equal(actual, expected, ignore_list=['dictionary_item_removed', 'iterable_item_removed']) -> None:
+	diff = DeepDiff(actual, expected)
+	diff_keys = list(diff.keys())
+	if diff_keys:
+		diff_keys = list(filter(lambda x: x not in ignore_list, diff_keys))
+		if len(diff_keys) == 0:
+			return
+
+	assert actual == expected
