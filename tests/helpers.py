c0cbb39d3f3f44f47f77126a0cff3edfb550f622
Ondra Krejčí <o.krejci13@gmail.com>
2022-11-23 00:24:07 +0100

    Add helper to convert NoSQL entities to JSON

--- tests/helpers.py
+++ tests/helpers.py
@@ -10,6 +10,11 @@ from bson import json_util
 
 from typing import Optional
 
+from app.entity.nosql import (
+	Location, Category, Author, Book, BookCopy, User, Borrowal, Reservation, Review,
+	EmbeddedCategory, EmbeddedBook, EmbeddedLocation, EmbeddedBookCopy, AuthorName, EmbeddedUser
+)
+
 def serialize(arg) -> str:
 	if isinstance(arg, list):
 		return json_util.dumps([x.to_mongo() for x in arg])
@@ -88,5 +93,135 @@ def find_by_id(id: int, arr: list):
 		return None
 	return find(lambda x: x['id'] == id, arr)
 
-def format_date(d: date) -> str:
+def format_date(d: Optional[date]) -> Optional[str]:
+	if d is None:
+		return None
 	return d.strftime('%Y-%m-%d')
+
+def to_json(x) -> dict:
+	if x is None:
+		return None
+	elif isinstance(x, list):
+		return map(lambda x: to_json(x), x)
+	elif isinstance(x, Location):
+		return {
+			'id': x.id,
+			'name': x.name,
+			'address': x.address
+		}
+	elif isinstance(x, Category):
+		return {
+			'id': x.id,
+			'name': x.name,
+			'description': x.description
+		}
+	elif isinstance(x, Author):
+		return {
+			'id': x.id,
+			'first_name': x.first_name,
+			'last_name': x.last_name,
+			'description': x.description,
+			'books': to_json(x.books)
+		}
+	elif isinstance(x, Book):
+		return {
+			'id': x.id,
+			'authors': to_json(x.authors),
+			'name': x.name,
+			'ISBN': x.ISBN,
+			'release_date': format_date(x.release_date),
+			'description': x.description,
+			'book_copies': to_json(x.book_copies),
+			'categories': to_json(x.categories)
+		}
+	elif isinstance(x, BookCopy):
+		return {
+			'id': x.id,
+			'book_id': x.book_id,
+			'print_date': format_date(x.print_date),
+			'note': x.note,
+			'state': x.state,
+			'location': to_json(x.location)
+		}
+	elif isinstance(x, User):
+		return {
+			'id': x.id,
+			'first_name': x.first_name,
+			'last_name': x.last_name,
+			'role': x.role,
+			'email': x.email
+		}
+	elif isinstance(x, Borrowal):
+		return {
+			'id': x.id,
+			'start_date': format_date(x.start_date),
+			'end_date': format_date(x.end_date),
+			'returned_date': format_date(x.returned_date),
+			'state': x.state,
+			'book_copy': to_json(x.book_copy),
+			'customer': to_json(x.customer),
+			'employee': to_json(x.employee),
+		}
+	elif isinstance(x, Reservation):
+		return {
+			'id': x.id,
+			'start_date': format_date(x.start_date),
+			'end_date': format_date(x.end_date),
+			'state': x.state,
+			'book_copy': to_json(x.book_copy),
+			'customer': to_json(x.customer)
+		}
+	elif isinstance(x, Review):
+		return {
+			'id': x.id,
+			'book_id': x.book_id,
+			'title': x.title,
+			'content': x.content,
+			'rating': x.rating,
+			'customer': to_json(x.customer)
+		}
+	elif isinstance(x, AuthorName):
+		return {
+			'id': x.id,
+			'first_name': x.first_name,
+			'last_name': x.last_name
+		},
+	elif isinstance(x, EmbeddedLocation):
+		return {
+			'id': x.id,
+			'name': x.name,
+			'address': x.address
+		}
+	elif isinstance(x, EmbeddedCategory):
+		return {
+			'id': x.id,
+			'name': x.name,
+			'description': x.description
+		}
+	elif isinstance(x, EmbeddedBookCopy):
+		return {
+			'id': x.id,
+			'book_id': x.book_id,
+			'print_date': format_date(x.print_date),
+			'note': x.note,
+			'state': x.state,
+			'location_id': x.location_id
+		}
+	elif isinstance(x, EmbeddedUser):
+		return {
+			'id': x.id,
+			'first_name': x.first_name,
+			'last_name': x.last_name,
+			'role': x.role,
+			'email': x.email
+		}
+	elif isinstance(x, EmbeddedBook):
+		return {
+			'id': x.id,
+			'name': x.name,
+			'ISBN': x.ISBN,
+			'release_date': format_date(x.release_date),
+			'description': x.description
+		}
+	else:
+		raise Exception('to_json: NOT IMPLEMENTED')
