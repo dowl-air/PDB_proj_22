940f1979ebba3daa6e0d2197935b2e030ae185fd
Ondra Krejčí <o.krejci13@gmail.com>
2022-11-22 11:35:05 +0100

    Test author operations

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -181,6 +181,100 @@ class TestLocation:
 		resp = client.delete('/location/%d/delete' % locationBrno.id)
 		expect_error(resp)
 
+class TestAuthor:
+	new_id: int
+
+	def test_author_add(self, client: FlaskClient):
+		data = {
+			'first_name': 'Karel',
+			'last_name': 'Čapek',
+			'description': 'A czech 20th century novellist...'
+		}
+
+		resp = client.post('/author/add', data=data)
+		assert resp.status_code == HTTPStatus.OK
+		json_data = loads(resp.data.decode())
+		assert 'id' in json_data
+
+		self.new_id = json_data['id']
+
+		resp = client.get('/author/%d' % self.new_id)
+		assert resp.status_code == HTTPStatus.OK
+		author = loads(resp.data.decode())
+		assert data['first_name'] == author['first_name']
+		assert data['last_name'] == author['last_name']
+		assert data['description'] == author['description']
+
+	def test_author_add_invalid(self, client: FlaskClient):
+		data = {
+			'first_name': 'Name',
+			'description': 'Missing last name'
+		}
+
+		resp = client.post('/author/add', data=data)
+		expect_error(resp)
+
+	def test_author_edit(self, client: FlaskClient):
+		data = {
+			'first_name': 'Edited author first name',
+			'last_name': 'Edited author last name',
+			'description': 'Edited author description'
+		}
+
+		resp = client.patch('/author/%d/edit' % self.new_id, data=data)
+		assert resp.status_code == HTTPStatus.OK
+
+		resp = client.get('/author/%d' % self.new_id)
+		assert resp.status_code == HTTPStatus.OK
+		author = loads(resp.data.decode())
+		assert data['first_name'] == author['first_name']
+		assert data['last_name'] == author['last_name']
+		assert data['description'] == author['description']
+
+	def test_author_edit_invalid(self, client: FlaskClient):
+		data = {
+			'first_name': None,
+			'last_name': 'Last',
+			'description': 'Invalid edit - no first name'
+		}
+
+		resp = client.patch('/author/%d/edit' % self.new_id, data=data)
+		expect_error(resp)
+
+	def test_author_edit_propagation(self, client: FlaskClient):
+		data = {
+			'first_name': 'Ray',
+			'last_name': 'Bradbury',
+			'description': 'Wrong author'
+		}
+
+		resp = client.patch('/author/%d/edit' % authorHuxley.id, data=data)
+		assert resp.status_code == HTTPStatus.OK
+
+		resp = client.get('/book/%d' % bookBraveNewWorld)
+		assert resp.status_code == HTTPStatus.OK
+		book = loads(resp.data.decode())
+		assert len(book['authors']) == 1
+		author = book['authors'][0]
+		assert author['first_name'] == data['first_name']
+		assert author['last_name'] == data['last_name']
+	
+	def test_author_delete(self, client: FlaskClient):
+		resp = client.delete('/author/%d/delete' % self.new_id)
+		assert resp.status_code == HTTPStatus.OK
+
+		resp = client.get('/author/%d' % self.new_id)
+		expect_error(resp)
+
+	def test_author_delete_propagation(self, client: FlaskClient):
+		resp = client.delete('/author/%d/delete' % authorHuxley.id)
+		assert resp.status_code == HTTPStatus.OK
+
+		resp = client.get('/book/%d' % bookBraveNewWorld.id)
+		assert resp.status_code == HTTPStatus.OK
+		book = loads(resp.data.decode())
+		assert len(book['authors']) == 0
+
 NEW_USER_EMAIL = 'new_email@email.cz'
 
 def test_register(client: FlaskClient):
