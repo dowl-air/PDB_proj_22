218cdaf0c84f9a795b9a9651a96c1a1e43f4a02f
Ondra Krejčí <o.krejci13@gmail.com>
2022-11-22 22:27:09 +0100

    Test: Book copy: update endpoints, fixes

--- tests/test_book_copy.py
+++ tests/test_book_copy.py
@@ -7,7 +7,8 @@ from json import loads
 
 from helpers import (
 	assert_error_response,
-	protected_post, protected_put, protected_delete
+	protected_post, protected_put, protected_delete,
+	format_date
 )
 from conftest import (
 	BOOK_COPY_STATE_GOOD, BOOK_COPY_STATE_DAMAGED,
@@ -26,21 +27,21 @@ class TestBookCopy:
 		data = {
 			'book_id': book1984.id,
 			'location_id': LOCATION.id,
-			'print_date': date(2019, 10, 5),
+			'print_date': format_date(date(2019, 10, 5)),
 			'note': 'Note',
 			'state': BOOK_COPY_STATE_GOOD
 		}
 
 		USER = userEmployeeBrno
 
-		resp = protected_post('/book_copy/add', data, client, USER)
+		resp = protected_post('/book-copies', data, client, USER)
 		assert resp.status_code == HTTPStatus.OK
 		json_data = loads(resp.data.decode())
 		assert 'id' in json_data
 
 		self.new_id = json_data['id']
 
-		resp = client.get('/book_copy/%d' % self.new_id)
+		resp = client.get('/book-copies/%d' % self.new_id)
 		assert resp.status_code == HTTPStatus.OK
 		copy = loads(resp.data.decode())
 		assert copy['book_id'] == data['book_id']
@@ -61,7 +62,7 @@ class TestBookCopy:
 		template = {
 			'book_id': BOOK.id,
 			'location_id': LOCATION.id,
-			'print_date': date(2019, 10, 5),
+			'print_date': format_date(date(2019, 10, 5)),
 			'note': 'Note',
 			'state': BOOK_COPY_STATE_GOOD
 		}
@@ -69,19 +70,19 @@ class TestBookCopy:
 		# missing book id
 		data = template.copy()
 		data['book_id'] = None
-		resp = protected_post('/book_copy/add', data, client, USER)
+		resp = protected_post('/book_copies', data, client, USER)
 		assert_error_response(resp)
 
 		# missing location id
 		data = template.copy()
 		data['location_id'] = None
-		resp = protected_post('/book_copy/add', data, client, USER)
+		resp = protected_post('/book_copies', data, client, USER)
 		assert_error_response(resp)
 
 		# missing print date
 		data = template.copy()
 		data['print_date'] = None
-		resp = protected_post('/book_copy/add', data, client, USER)
+		resp = protected_post('/book_copies', data, client, USER)
 		assert_error_response(resp)
 
 	def test_book_copy_edit(self, client: FlaskClient):
@@ -93,15 +94,15 @@ class TestBookCopy:
 		data = {
 			'book_id': BOOK.id,
 			'location_id': LOCATION.id,
-			'print_date': date(2018, 7, 20),
+			'print_date': format_date(date(2018, 7, 20)),
 			'note': 'Edited note',
 			'state': BOOK_COPY_STATE_DAMAGED
 		}
 
-		resp = protected_put('/book_copy/%d/edit' % self.new_id, data, client, USER)
+		resp = protected_put('/book-copies/%d' % self.new_id, data, client, USER)
 		assert resp.status_code == HTTPStatus.OK
 
-		resp = client.get('/book_copy/%d' % self.new_id)
+		resp = client.get('/book-copies/%d' % self.new_id)
 		assert resp.status_code == HTTPStatus.OK
 		copy = loads(resp.data.decode())
 		assert data['book_id'] == copy['book_id']
@@ -122,7 +123,7 @@ class TestBookCopy:
 		template = {
 			'book_id': BOOK.id,
 			'location_id': LOCATION.id,
-			'print_date': date(2019, 10, 5),
+			'print_date': format_date(date(2019, 10, 5)),
 			'note': 'Note',
 			'state': BOOK_COPY_STATE_GOOD
 		}
@@ -130,19 +131,19 @@ class TestBookCopy:
 		# missing book id
 		data = template.copy()
 		data['book_id'] = None
-		resp = protected_post('/book_copy/%d/edit' % self.new_id, data, client, USER)
+		resp = protected_put('/book-copies/%d' % self.new_id, data, client, USER)
 		assert_error_response(resp)
 
 		# missing location id
 		data = template.copy()
 		data['location_id'] = None
-		resp = protected_post('/book_copy/%d/edit' % self.new_id, data, client, USER)
+		resp = protected_put('/book-copies/%d' % self.new_id, data, client, USER)
 		assert_error_response(resp)
 
 		# missing print date
 		data = template.copy()
 		data['print_date'] = None
-		resp = protected_post('/book_copy/%d/edit' % self.new_id, data, client, USER)
+		resp = protected_put('/book-copies/%d' % self.new_id, data, client, USER)
 		assert_error_response(resp)
 
 	def test_book_copy_edit_propagation(self, client: FlaskClient):
@@ -155,12 +156,12 @@ class TestBookCopy:
 		data = {
 			'book_id': BOOK.id,
 			'location_id': LOCATION.id,
-			'print_date': date(2017, 4, 16),
+			'print_date': format_date(date(2017, 4, 16)),
 			'note': 'Edited note',
 			'state': BOOK_COPY_STATE_DAMAGED
 		}
 
-		resp = protected_put('/book_copy/%d/edit' % BOOK_COPY.id, data, client, USER)
+		resp = protected_put('/book-copies/%d' % BOOK_COPY.id, data, client, USER)
 		assert resp.status_code == HTTPStatus.OK
 
 		resp = client.get('/book/%d' % BOOK.id)
@@ -178,10 +179,10 @@ class TestBookCopy:
 	def test_book_copy_delete(self, client: FlaskClient):
 		USER = userEmployeeBrno
 
-		resp = protected_delete('/book_copy/%d/delete' % self.new_id, client, USER)
+		resp = protected_delete('/book-copies/%d' % self.new_id, client, USER)
 		assert resp.status_code == HTTPStatus.OK
 
-		resp = client.get('/book_copy/%d' % self.new_id)
+		resp = client.get('/book-copies/%d' % self.new_id)
 		assert_error_response(resp)
 
 	# cannot delete book copy with borrowals
@@ -189,7 +190,7 @@ class TestBookCopy:
 		USER = userEmployeeBrno
 		BOOK_COPY = bc1984London1
 
-		resp = protected_delete('/book_copy/%d/delete' % BOOK_COPY.id, client, USER)
+		resp = protected_delete('/book-copies/%d' % BOOK_COPY.id, client, USER)
 		assert_error_response(resp)
 
 	def test_book_copy_delete_propagation(self, client: FlaskClient):
@@ -198,11 +199,11 @@ class TestBookCopy:
 		BOOK_COPY = bc1984London2
 		BOOK = book1984
 
-		resp = protected_delete('/book_copy/%d/delete' % BOOK_COPY.id, client, USER)
+		resp = protected_delete('/book-copies/%d' % BOOK_COPY.id, client, USER)
 		assert resp.status_code == HTTPStatus.OK
 
 		resp = client.get('/book/%d' % BOOK.id)
 		assert resp.status_code == HTTPStatus.OK
 		book = loads(resp.data.decode())
-		copies = list(filter(lambda x: x['id'] == BOOK_COPY.id, copies))
+		copies = list(filter(lambda x: x['id'] == BOOK_COPY.id, book['copies']))
 		assert len(copies) == 0
