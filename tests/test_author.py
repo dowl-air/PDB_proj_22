eab5d99b6629c4bfbe27f9d797c513622f9035c3
Ondra Krejčí <o.krejci13@gmail.com>
2022-11-22 22:06:22 +0100

    Test: Author: update endpoints, fixes

--- tests/test_author.py
+++ tests/test_author.py
@@ -5,8 +5,9 @@ from http import HTTPStatus
 from json import loads
 
 from helpers import (
+	protected_post, protected_put, protected_delete,
 	assert_error_response,
-	protected_post, protected_put, protected_delete
+	find_by_id
 )
 from conftest import (
 	authorHuxley,
@@ -18,22 +19,22 @@ class TestAuthor:
 	new_id: int = 0
 
 	def test_author_add(self, client: FlaskClient):
+		USER = userEmployeeBrno
+
 		data = {
 			'first_name': 'Karel',
 			'last_name': 'Čapek',
 			'description': 'A czech 20th century novellist...'
 		}
 
-		USER = userEmployeeBrno
-
-		resp = protected_post('/author/add', data, client, USER)
+		resp = protected_post('/authors', data, client, USER)
 		assert resp.status_code == HTTPStatus.OK
 		json_data = loads(resp.data.decode())
 		assert 'id' in json_data
 
 		self.new_id = json_data['id']
 
-		resp = client.get('/author/%d' % self.new_id)
+		resp = client.get('/authors/%d' % self.new_id)
 		assert resp.status_code == HTTPStatus.OK
 		author = loads(resp.data.decode())
 		assert data['first_name'] == author['first_name']
@@ -41,29 +42,29 @@ class TestAuthor:
 		assert data['description'] == author['description']
 
 	def test_author_add_invalid(self, client: FlaskClient):
+		USER = userEmployeeBrno
+
 		data = {
 			'first_name': 'Name',
 			'description': 'Missing last name'
 		}
 
-		USER = userEmployeeBrno
-
-		resp = protected_post('/author/add', data, client, USER)
+		resp = protected_post('/authors', data, client, USER)
 		assert_error_response(resp)
 
 	def test_author_edit(self, client: FlaskClient):
+		USER = userEmployeeBrno
+
 		data = {
 			'first_name': 'Edited author first name',
 			'last_name': 'Edited author last name',
 			'description': 'Edited author description'
 		}
 
-		USER = userEmployeeBrno
-
-		resp = protected_put('/author/%d/edit' % self.new_id, data, client, USER)
+		resp = protected_put('/authors/%d' % self.new_id, data, client, USER)
 		assert resp.status_code == HTTPStatus.OK
 
-		resp = client.get('/author/%d' % self.new_id)
+		resp = client.get('/authors/%d' % self.new_id)
 		assert resp.status_code == HTTPStatus.OK
 		author = loads(resp.data.decode())
 		assert data['first_name'] == author['first_name']
@@ -71,30 +72,33 @@ class TestAuthor:
 		assert data['description'] == author['description']
 
 	def test_author_edit_invalid(self, client: FlaskClient):
+		USER = userEmployeeBrno
+
 		data = {
 			'first_name': None,
 			'last_name': 'Last',
 			'description': 'Invalid edit - no first name'
 		}
 
-		USER = userEmployeeBrno
-
-		resp = protected_put('/author/%d/edit' % self.new_id, data, client, USER)
+		resp = protected_put('/authors/%d' % self.new_id, data, client, USER)
 		assert_error_response(resp)
 
 	def test_author_edit_propagation(self, client: FlaskClient):
+		USER = userEmployeeBrno
+
+		AUTHOR = authorHuxley
+		BOOK = bookBraveNewWorld
+
 		data = {
 			'first_name': 'Ray',
 			'last_name': 'Bradbury',
 			'description': 'Wrong author'
 		}
 
-		USER = userEmployeeBrno
-
-		resp = protected_put('/author/%d/edit' % authorHuxley.id, data, client, USER)
+		resp = protected_put('/authors/%d' % AUTHOR.id, data, client, USER)
 		assert resp.status_code == HTTPStatus.OK
 
-		resp = client.get('/book/%d' % bookBraveNewWorld)
+		resp = client.get('/books/%d' % BOOK)
 		assert resp.status_code == HTTPStatus.OK
 		book = loads(resp.data.decode())
 		assert len(book['authors']) == 1
@@ -105,19 +109,27 @@ class TestAuthor:
 	def test_author_delete(self, client: FlaskClient):
 		USER = userEmployeeBrno
 
-		resp = protected_delete('/author/%d/delete' % self.new_id, client, USER)
+		resp = protected_delete('/authors/%d' % self.new_id, client, USER)
 		assert resp.status_code == HTTPStatus.OK
 
-		resp = client.get('/author/%d' % self.new_id)
+		resp = client.get('/authors/%d' % self.new_id)
 		assert_error_response(resp)
 
 	def test_author_delete_propagation(self, client: FlaskClient):
 		USER = userEmployeeBrno
 
-		resp = protected_delete('/author/%d/delete' % authorHuxley.id, client, USER)
+		BOOK = bookBraveNewWorld
+		AUTHOR = authorHuxley
+
+		resp = client.get('/books/%d' % BOOK.id)
+		assert resp.status_code == HTTPStatus.OK
+		book = loads(resp.data.decode())
+		assert find_by_id(AUTHOR.id, book['authors']) is not None
+
+		resp = protected_delete('/authors/%d' % AUTHOR.id, client, USER)
 		assert resp.status_code == HTTPStatus.OK
 
-		resp = client.get('/book/%d' % bookBraveNewWorld.id)
+		resp = client.get('/books/%d' % BOOK.id)
 		assert resp.status_code == HTTPStatus.OK
 		book = loads(resp.data.decode())
 		assert len(book['authors']) == 0
