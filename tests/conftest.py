27a63069d048dc3c2a7a02b9c28344c31c7a8b24
Ondra Krejčí <o.krejci13@gmail.com>
2022-11-23 20:52:50 +0100

    Clean up test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 
 import pytest
+from flask.testing import FlaskClient
 
 from app.create_app import create_app, db, mongo
 
@@ -8,19 +9,16 @@ from data import (
 	SQL_LOCATIONS, SQL_CATEGORIES, SQL_AUTHORS, SQL_BOOKS, SQL_BOOK_COPIES, SQL_USERS, SQL_BORROWALS, SQL_RESERVATIONS, SQL_REVIEWS
 )
 
+MONGO_DB_NAME = 'pdb'
+
 @pytest.fixture(scope='session', autouse=True)
-def clear_db():
+def clear_db() -> None:
 	with create_app().app_context():
 		db.drop_all()
-		mongo.connection['default'].drop_database('pdb')
-
-@pytest.fixture
-def client():
-	with create_app().test_client() as client:
-		yield client
+		mongo.connection['default'].drop_database(MONGO_DB_NAME)
 
 @pytest.fixture(scope='session', autouse=True)
-def fill_db():
+def fill_db() -> None:
 	with create_app().app_context():
 		for arr in [LOCATIONS, CATEGORIES, AUTHORS, BOOKS, BOOK_COPIES, USERS, BORROWALS, RESERVATIONS, REVIEWS]:
 			for it in arr:
@@ -31,3 +29,7 @@ def fill_db():
 			for it in arr:
 				db.session.add(it)
 			db.session.commit()
+
+@pytest.fixture
+def client() -> FlaskClient:
+	return create_app().test_client()
