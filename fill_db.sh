539fd30760039d5a686d8b7d261663b7be789c55
Ondra Krejčí <o.krejci13@gmail.com>
2022-11-28 22:20:33 +0100

    Correction

--- fill_db.sh
+++ fill_db.sh
@@ -4,6 +4,7 @@ cd "$(dirname "$0")"
 
 if [ $# -gt 0 ] && [ "$1" = "-l" -o "$1" = "--local" ]; then
 	export DB_HOST="localhost"
+
 	export MONGODB_USERNAME=""
 	export MONGODB_PASSWORD=""
 	export MONGODB_HOSTNAME="localhost"
